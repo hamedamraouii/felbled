7115b8e579e77e0da273a8b4dd4fcfb810561a7d
hamedamraouii <hamed.amraoui@iteam-univ.tn>
2025-07-01 15:25:16 +0200

    first commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/login', authController.loginAdmin);
 router.post('/register', authController.registerAdmin);
-router.post('/refresh', authController.refreshToken); // New route for token refresh
+
 router.post('/logout', authMiddleware, authController.logoutAdmin);
 
 module.exports = router;
